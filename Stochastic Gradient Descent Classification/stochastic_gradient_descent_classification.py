7322406d15308cea5433488a8da13143e3491e2a
Tongs <62232130+thetongs@users.noreply.github.com>
2022-07-03 12:43:35 +0530

    Add files via upload

---
+++ Stochastic Gradient Descent Classification/stochastic_gradient_descent_classification.py
@@ -0,0 +1,60 @@
+# Load Libraries
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.utils import to_categorical
+from sklearn.datasets import make_blobs
+from matplotlib import pyplot
+
+# generate 2d classification dataset
+X, y = make_blobs(n_samples=1000, centers=3, n_features=2, cluster_std=2, random_state=2)
+
+# one hot encode output variable
+y = to_categorical(y)
+
+# split into train and test
+n_train = 500
+trainX, testX = X[:n_train, :], X[n_train:, :]
+trainy, testy = y[:n_train], y[n_train:]
+
+# define model
+model = Sequential()
+model.add(Dense(50, input_dim=2, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(3, activation='softmax'))
+
+# compile model
+opt = SGD(learning_rate=0.01, momentum=0.9)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+# fit model
+history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=200, verbose=0,
+batch_size=1)
+
+# evaluate the model
+_, train_acc = model.evaluate(trainX, trainy, verbose=0)
+_, test_acc = model.evaluate(testX, testy, verbose=0)
+
+print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
+# plot loss learning curves
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+# show the plot
+pyplot.title("SGD Cross Entropy Loss vs Training Epochs")
+pyplot.xlabel("Epochs")
+pyplot.ylabel("Loss")
+pyplot.legend()
+pyplot.show()
+# Train: 0.708, Test: 0.684
+
+# plot accuracy learning curves
+
+pyplot.title('Accuracy')
+pyplot.plot(history.history['accuracy'], label='train')
+pyplot.plot(history.history['val_accuracy'], label='test')
+
+pyplot.title("SGD Accuracy epochs")
+pyplot.xlabel("Epochs")
+pyplot.ylabel("Accuracy")
+pyplot.legend()
+pyplot.show()
+
